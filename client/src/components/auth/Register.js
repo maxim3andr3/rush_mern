ab7ea60b1627947b4efca61b31582629d6835818
maxim3andr3 <maxim3andr3@gmail.com>
2019-09-02 15:41:22 +0200

    Modified auth/Register.js : adding the register field

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,12 +1,12 @@
 import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
-import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types'
 
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register }) => {
     const [formData, setFormData] = useState({
         name: '',
         email:'',
@@ -15,29 +15,16 @@ const Register = ({ setAlert }) => {
     });
 
     const { name, email, password, password2 } = formData;
-    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const onChange = e =>
+      setFormData({ ...formData, [e.target.name]: e.target.value });
+
     const onSubmit = async e => {
         e.preventDefault();
         if(password !== password2) {
             setAlert('Passwords do not match', 'danger');
-        }else{
-            const newUser = {
-                name,
-                email,
-                password
-            }
-            try{
-                const config = {
-                    headers: {
-                        'content-Type': 'application/json'
-                    }
-                }
-                const body = JSON.stringify(newUser);
-                const res = await axios.post('/api/users', body, config);
-                console.log(res.data);
-            } catch (err){
-                console.error(err.response.data);
-            }
+        } else {
+          register({ name, email, password });
         }
     };
 
@@ -46,10 +33,10 @@ const Register = ({ setAlert }) => {
       <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
       <form className="form" onSubmit={e => onSubmit(e)}>
         <div className="form-group">
-          <input type='text' placeholder="Name" name="name" value = {name} onChange={e => onChange(e)} required />
+          <input type='text' placeholder="Name" name="name" value = {name} onChange={e => onChange(e)} />
         </div>
         <div className="form-group">
-          <input type="email" placeholder="Email Address" name="email" value = {email} onChange={e => onChange(e)} required />
+          <input type="email" placeholder="Email Address" name="email" value = {email} onChange={e => onChange(e)} />
           <small className="form-text"></small>
         </div>
         <div className="form-group">
@@ -82,6 +69,7 @@ const Register = ({ setAlert }) => {
 
 Register.propTypes = {
   setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired
 }
 
-export default connect(null, { setAlert })(Register);
+export default connect(null, { setAlert, register })(Register);
