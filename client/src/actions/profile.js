600ed0a3aef204cce3fe0887570d10df73ec5316
maxim3andr3 <maxim3andr3@gmail.com>
2019-09-04 12:37:25 +0200

    Modified actions/profile.js : adding a getter to all profiles and a single profile by id

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -3,7 +3,10 @@ import { setAlert } from './alert';
 
 import {
     GET_PROFILE,
+    GET_PROFILES,
     PROFILE_ERROR,
+    CLEAR_PROFILE,
+    ACCOUNT_DELETED,
     // UPDATE_PROFILE
 } from './types';
 
@@ -11,6 +14,42 @@ import {
 export const getCurrentProfile = () => async dispatch => {
     try {
         const res = await axios.get('/api/profile/me');
+
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        })
+    } catch (err) {
+        dispatch({
+            type:PROFILE_ERROR,
+            payload: {msg: err.response.statusText, status: err.response.status}
+        });
+    }
+};
+
+// Get all profiles
+export const getProfiles = () => async dispatch => {
+    dispatch({ type: CLEAR_PROFILE });
+
+    try {
+        const res = await axios.get('/api/profile');
+
+        dispatch({
+            type: GET_PROFILES,
+            payload: res.data
+        })
+    } catch (err) {
+        dispatch({
+            type:PROFILE_ERROR,
+            payload: {msg: err.response.statusText, status: err.response.status}
+        });
+    }
+};
+
+// Get profile by ID
+export const getProfileById = userId => async dispatch => {
+    try {
+        const res = await axios.get(`/api/profile/user/${userId}`);
 
         dispatch({
             type: GET_PROFILE,
@@ -59,6 +98,25 @@ export const createProfile = (formData, history, edit = false) => async dispatch
         });  
     }
 };
+
+// Delete account & profile
+export const deleteAccount = () => async dispatch => {
+    if (window.confirm('Are you sure ? This can NOT be undone !')) {
+        try {
+            await axios.delete('/api/profile');
+
+            dispatch({ type: CLEAR_PROFILE });
+            dispatch({ type: ACCOUNT_DELETED });
+
+            dispatch(setAlert('Your account has been permanantly deleted'));
+        } catch (err) {
+            dispatch({
+                type: PROFILE_ERROR,
+                payload: { msg: err.response.statusText, status: err.response.status }
+            });
+        }
+    }
+}
 
 // Add Experience
 // export const addExperience = (formData, history) => async dispatch => {
