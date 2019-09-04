b1d3fc8e7259eaa6c2309cc62a73fc14f0afe58d
maxim3andr3 <maxim3andr3@gmail.com>
2019-09-04 10:19:27 +0200

    Modified actions/profile.js : commenting the AddExperience and AddEducation route

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -4,7 +4,7 @@ import { setAlert } from './alert';
 import {
     GET_PROFILE,
     PROFILE_ERROR,
-    UPDATE_PROFILE
+    // UPDATE_PROFILE
 } from './types';
 
 // get current users profile
@@ -61,67 +61,67 @@ export const createProfile = (formData, history, edit = false) => async dispatch
 };
 
 // Add Experience
-export const addExperience = (formData, history) => async dispatch => {
-    try {
-        const config = {
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }
+// export const addExperience = (formData, history) => async dispatch => {
+//     try {
+//         const config = {
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             }
+//         }
 
-        const res = await axios.put('/api/profile/experience', formData, config);
+//         const res = await axios.put('/api/profile/experience', formData, config);
      
-        dispatch({
-        type: UPDATE_PROFILE,
-        payload: res.data
-        });
+//         dispatch({
+//         type: UPDATE_PROFILE,
+//         payload: res.data
+//         });
 
-        dispatch(setAlert('Experience Added', 'success'));
+//         dispatch(setAlert('Experience Added', 'success'));
 
-        history.push('/dashboard');
-    } catch (err) {
-        const errors = err.response.data.errors;
+//         history.push('/dashboard');
+//     } catch (err) {
+//         const errors = err.response.data.errors;
 
-        if (errors) {
-            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
-        }
+//         if (errors) {
+//             errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+//         }
 
-        dispatch({
-            type:PROFILE_ERROR,
-            payload: { msg: err.response.statusText, status: err.response.status }
-        });  
-    }
-}
+//         dispatch({
+//             type:PROFILE_ERROR,
+//             payload: { msg: err.response.statusText, status: err.response.status }
+//         });  
+//     }
+// }
 
 // Add Education
-export const addEducation = (formData, history) => async dispatch => {
-    try {
-        const config = {
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }
+// export const addEducation = (formData, history) => async dispatch => {
+//     try {
+//         const config = {
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             }
+//         }
 
         
-        const res = await axios.put('/api/profile/education', formData, config);
+//         const res = await axios.put('/api/profile/education', formData, config);
      
-        dispatch({
-        type: UPDATE_PROFILE,
-        payload: res.data
-        });
+//         dispatch({
+//         type: UPDATE_PROFILE,
+//         payload: res.data
+//         });
 
-        dispatch(setAlert('Education Added', 'success'));
-        history.push('/dashboard');
-    } catch (err) {
-        const errors = err.response.data.errors;
+//         dispatch(setAlert('Education Added', 'success'));
+//         history.push('/dashboard');
+//     } catch (err) {
+//         const errors = err.response.data.errors;
 
-        if (errors) {
-            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
-        }
+//         if (errors) {
+//             errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+//         }
 
-        dispatch({
-            type:PROFILE_ERROR,
-            payload: {msg: err.response.statusText, status: err.response.status}
-        });  
-    }
-}
+//         dispatch({
+//             type:PROFILE_ERROR,
+//             payload: {msg: err.response.statusText, status: err.response.status}
+//         });  
+//     }
+// }
