69f4794158a464759220ed3ae8267f4777237052
maxim3andr3 <maxim3andr3@gmail.com>
2019-09-04 19:09:41 +0200

    Added ProfileItem.js

---
+++ client/src/components/profiles/ProfileItem.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const ProfileItem = ({
+    profile: {
+        // user: { _id, name, avatar },
+        firstname,
+        lastname,
+        location
+    }
+}) => {
+    return (
+        <div className="profile bg-light">
+            {/* <img src={avatar} alt="" className="round-img" /> */}
+            <div>
+                {/* <h2>{name}</h2> */}
+                <p>{firstname} {lastname}</p>
+                <p className="my-1">{location}</p>
+                {/* <Link to={`/profile/${_id}`} className="btn btn-primary"> */}
+                    View profile
+                {/* </Link> */}
+            </div>
+        </div>
+    )
+}
+
+ProfileItem.propTypes = {
+    profile: PropTypes.object.isRequired,
+}
+
+export default ProfileItem;
