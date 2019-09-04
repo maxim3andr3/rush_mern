86f074acdd3fb8b0d7b6cf513a9445e5baab7f97
lisa <lisa.toulzac@coding-academy.fr>
2019-09-04 15:12:01 +0200

    css

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -20,11 +20,16 @@ const Dashboard = ({
     <Spinner />
     ) : (
         <Fragment>
-        <div className="my-2">
+        <div className="row">
+        <div className="test">
+            <p>gauche</p>
+            <div className="my-2">
             <button className="btn btn-danger" onClick={() => deleteAccount()}>
                 <i className="fas fa-user-minus"></i> Delete my account
             </button>
         </div>
+        </div>
+        <div className="test">
         <h1 className="large text-primary">Dashboard</h1>
         <p className="lead">
             <i className="fas fa-user"></i> Welcome { user && user.name}</p>
@@ -37,6 +42,8 @@ const Dashboard = ({
                     <p>You have not yet setup a profile, please add some info</p>
                     <Link to='/create-profile' className="btn btn-primary my-1">Create Profile</Link>
                 </Fragment>)}
+        </div>
+        </div>
         </Fragment>
     );
 };
