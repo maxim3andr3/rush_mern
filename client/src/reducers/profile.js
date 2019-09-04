f648773da48081046ca9f4f2e63d30368fa0bf6d
maxim3andr3 <maxim3andr3@gmail.com>
2019-09-04 10:22:48 +0200

    Modified reducers/profile.js : commenting the update_profile

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -2,7 +2,7 @@ import {
     GET_PROFILE, 
     PROFILE_ERROR, 
     CLEAR_PROFILE,
-    UPDATE_PROFILE
+    // UPDATE_PROFILE
  } from "../actions/types";
 
 const initialState = {
@@ -18,7 +18,7 @@ export default function(state = initialState, action) {
 
     switch(type){
         case GET_PROFILE:
-        case UPDATE_PROFILE:
+        // case UPDATE_PROFILE:
             return {
                 ...state,
                 profile: payload,
