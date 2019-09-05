cb9dac8c63ec0d402649243d99b8be83fe339ac7
maxim3andr3 <maxim3andr3@gmail.com>
2019-09-05 10:34:30 +0200

    Modified reducers/profile.js : removing console.log function

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -15,7 +15,7 @@ const initialState = {
 };
 
 export default function(state = initialState, action) {
-    const { type, payload} = action;
+    const { type, payload } = action;
 
     switch(type){
         case GET_PROFILE:
