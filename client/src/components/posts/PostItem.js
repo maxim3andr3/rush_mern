21effb9ab299ebc251064e125f582421e3261a0b
maxim3andr3 <maxim3andr3@gmail.com>
2019-09-05 17:58:21 +0200

    Modified components/posts/PostItem.js : adding a showActions props

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -5,7 +5,13 @@ import Moment from 'react-moment';
 import { connect } from 'react-redux';
 import { addLike, removeLike, deletePost } from '../../actions/post';
 
-const PostItem = ({ addLike, removeLike, deletePost, auth, post: { _id, text, name, avatar, user, likes, comments, date }
+const PostItem = ({
+  addLike,
+  removeLike,
+  deletePost,
+  auth,
+  post: { _id, text, name, avatar, user, likes, comments, date },
+  showActions
 }) => (
   <Fragment>
   <div className="post bg-white p-1 my-1">
@@ -26,6 +32,8 @@ const PostItem = ({ addLike, removeLike, deletePost, auth, post: { _id, text, na
         <p className="post-date">
           Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
       </p>
+
+      {showActions && <Fragment>
       <button onClick={e => addLike(_id)} type="button" className="btn btn-light">
         <i className="fas fa-thumbs-up"></i>{' '}
         <span>{likes.length > 0 && (
@@ -35,7 +43,7 @@ const PostItem = ({ addLike, removeLike, deletePost, auth, post: { _id, text, na
       <button  onClick={e => removeLike(_id)} type="button" className="btn btn-light">
         <i className="fas fa-thumbs-down"></i>
       </button>
-      <Link to={`/post/${_id}`} className="btn btn-primary">
+      <Link to={`/posts/${_id}`} className="btn btn-primary">
         Discussion {comments.length > 0 && (
             <span className='comment-count'>{comments.length}</span>
         )}
@@ -49,10 +57,15 @@ const PostItem = ({ addLike, removeLike, deletePost, auth, post: { _id, text, na
         <i className="fas fa-times"></i>
         </button>
       )}
+      </Fragment>}
     </div>
   </div>
   </Fragment>
 );
+
+PostItem.defaultProps = {
+  showActions: true
+};
 
 PostItem.propTypes = {
     post: PropTypes.object.isRequired,
