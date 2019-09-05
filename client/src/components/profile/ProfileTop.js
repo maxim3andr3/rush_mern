6f65e9fb74c77ea1192d0fea37b9ba68cc0fcec4
maxim3andr3 <maxim3andr3@gmail.com>
2019-09-05 10:29:54 +0200

    Modified components/profile/ProfileTop.js : changing some class tag by className

--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -10,34 +10,34 @@ const ProfileTop = ({ profile: {
     user: { name, avatar }
 } }) => {
     return (
-        <div class="profile-top bg-primary p-2">
+        <div className="profile-top bg-primary p-2">
           <img
-            class="round-img my-1"
+            className="round-img my-1"
             src={ avatar }
             alt=""
           />
-          <h1 class="large">{ firstname } { lastname }</h1>
-          <p class="lead">{ website && <span>{ website }</span> }</p>
+          <h1 className="large">{ firstname } { lastname }</h1>
+          <p className="lead">{ website && <span>{ website }</span> }</p>
           <p>{ location && <span>{ location }</span> }</p>
-          <div class="icons my-1">
+          <div className="icons my-1">
             { website && (
                 <a href={website} target="_blank" rel="noopener noreferrer">
-                    <i class="fas fa-globe fa-2x"></i>
+                    <i className="fas fa-globe fa-2x"></i>
                 </a>
             )}
             { social && social.twitter && (
                 <a href={ social.twitter } target="_blank" rel="noopener noreferrer">
-                    <i class="fab fa-twitter fa-2x"></i>
+                    <i className="fab fa-twitter fa-2x"></i>
                 </a>
             )}
             { social && social.facebook && (
                 <a href={ social.facebook } target="_blank" rel="noopener noreferrer">
-                    <i class="fab fa-facebook fa-2x"></i>
+                    <i className="fab fa-facebook fa-2x"></i>
                 </a>
             )}
             { social && social.linkedin && (
                 <a href={ social.linkedin } target="_blank" rel="noopener noreferrer">
-                    <i class="fab fa-linkedin fa-2x"></i>
+                    <i className="fab fa-linkedin fa-2x"></i>
                 </a>
             )}
           </div>
