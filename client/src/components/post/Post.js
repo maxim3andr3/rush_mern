f37c57af10349de124e7da80500d342821939a97
maxim3andr3 <maxim3andr3@gmail.com>
2019-09-05 19:11:06 +0200

    Modified components/post/Post.js : adding the CommentForm tag

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -5,11 +5,15 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import PostItem from '../posts/PostItem';
 import { getPost } from '../../actions/post';
+import CommentForm from './CommentForm';
+import CommentItem from './CommentItem';
+
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
     useEffect(() => {
         getPost(match.params.id);
     }, [getPost]);
+
     return loading || post === null ? (
     <Spinner />
     ) : (
@@ -18,6 +22,12 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
             Back to posts
         </Link>
         <PostItem post={post} showActions={false} />
+        <CommentForm postId={post._id} />
+        <div className="comments">
+            {post.comments.map(comment => (
+                <CommentItem key={comment._id} comment={comment} postId={post._id} />
+            ))}
+        </div>
     </Fragment>
     );
 };
