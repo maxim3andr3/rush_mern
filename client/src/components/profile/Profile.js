5154eab0687696a015e76c5d17cb6b79950d6845
maxim3andr3 <maxim3andr3@gmail.com>
2019-09-05 10:29:24 +0200

    Modified components/profile/Profile.js : changing some class tag by className

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -19,14 +19,14 @@ const Profile = ({
     return (
         <Fragment>
             {profile === null || loading ? (<Spinner />) : (<Fragment>
-                <Link to='/profiles' classname="btn btn-light">
+                <Link to='/profiles' className="btn btn-light">
                     Back to profiles
                 </Link>
                 {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (<Link to="/edit/profile" className="btn btn-dark">
                     Edit profile
                 </Link>
                 )}
-                <div class="profile-grid my-1">
+                <div className="profile-grid my-1">
                     <ProfileTop profile={profile} />
                     <ProfileAbout profile={profile} />
                 </div>
