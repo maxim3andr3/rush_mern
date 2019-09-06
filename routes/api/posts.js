5bb5d1fce830eca2c1a77d291fdec7508fd7dcef
vincent <vincent.nguyen@coding-academy.fr>
2019-09-06 16:20:06 +0200

    updates api route for likes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -153,7 +153,7 @@ router.put('/unlike/:id', auth, async(req, res) => {
         const post = await Post.findById(req.params.id);
 
         // check if the post has already been liked
-        if (post.likes.filter(like => like.user.toString() === req.user.id).length = 0) {
+        if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
             return res.status(400).json({ msg: 'Post has not yet been liked' });
         }
 
